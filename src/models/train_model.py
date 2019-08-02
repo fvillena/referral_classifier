eea66760d4128f916c5c0d5938f1f039c216ab60
Fabian Villena <villenafabian@gmail.com>
2019-08-02 13:48:45 -0400

    model trainer

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,19 +1,70 @@
 import sklearn.neural_network
 import sklearn.metrics
 import joblib
+import numpy as np
+import json
 
-serialized_model_file = '../../models/serialized_models/model.joblib'
-performance_file = '../../models/model_performances/model.txt'
+serialized_model_file = '../../models/serialized_models/model_waiting_list.joblib'
+performance_file = '../../models/model_performances/model_waiting_list.json'
 
-train_texts = []
+train_texts_location = '../../data/processed/train_text_waiting_list.npy'
+train_ages_location = '../../data/processed/train_age.txt'
+train_labels_location = '../../data/processed/train_labels.txt'
+test_texts_location = '../../data/processed/test_text_waiting_list.npy'
+test_ages_location = '../../data/processed/test_age.txt'
+test_labels_location = '../../data/processed/test_labels.txt'
+
+print('loading data')
+
+train_texts = np.load(train_texts_location)
+
 train_ages = []
-train_features = []
-train_labels = []
+with open(train_ages_location, encoding='utf-8') as file:
+    for line in file:
+        line = line.rstrip()
+        train_ages.append(float(line))
+train_ages = np.asarray([train_ages]).T
 
-test_texts = []
+
+train_labels = []
+with open(train_labels_location, encoding='utf-8') as file:
+    for line in file:
+        line = line.rstrip()
+        if line == 'True':
+            train_labels.append(True)
+        else:
+            train_labels.append(False)
+train_labels = np.asarray([train_labels]).T
+
+train = np.concatenate([train_texts, train_ages, train_labels], axis=1)
+
+train = train[~np.isnan(train).any(axis=1)]
+
+
+test_texts = np.load(test_texts_location)
+
 test_ages = []
-test_features = []
+with open(test_ages_location, encoding='utf-8') as file:
+    for line in file:
+        line = line.rstrip()
+        test_ages.append(float(line))
+test_ages = np.asarray([test_ages]).T
+
+
 test_labels = []
+with open(test_labels_location, encoding='utf-8') as file:
+    for line in file:
+        line = line.rstrip()
+        if line == 'True':
+            test_labels.append(True)
+        else:
+            test_labels.append(False)
+test_labels = np.asarray([test_labels]).T
+
+test = np.concatenate([test_texts, test_ages, test_labels], axis=1)
+test = test[~np.isnan(test).any(axis=1)]
+
+print('training model')
 
 classifier = sklearn.neural_network.MLPClassifier(
     verbose=True,
@@ -25,19 +76,23 @@ classifier = sklearn.neural_network.MLPClassifier(
 )
 
 classifier.fit(
-    train_features,
-    train_labels
+    train[:,:301],
+    train[:,301]
 )
 
+print('measuring performance')
+
 performance = sklearn.metrics.classification_report(
-    train_labels,
+    test[:,301],
     classifier.predict(
-        test_features
+        test[:,:301]
     ),
     output_dict = True
 )
 
+print('saving files')
+
 with open(performance_file,'w') as file:
-    file.write(performance)
+    json.dump(performance, file, indent=1)
 
 joblib.dump(classifier, serialized_model_file) 
