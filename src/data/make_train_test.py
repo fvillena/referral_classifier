1f1b2ab7a3941475843704e2d9b48a7fdb48b477
Fabian Villena <villenafabian@gmail.com>
2019-08-07 14:07:03 -0400

    only 1 training dataset for later cross-validation

--- src/data/make_train_test.py
+++ src/data/make_train_test.py
@@ -43,46 +43,58 @@ scaler = sklearn.preprocessing.MinMaxScaler()
 data["edad"] = scaler.fit_transform(data[["edad"]])
 data['GES'] = np.where(data['GES'] == 'SI', True, False)
 data = data[data.SOSPECHA_DIAGNOSTICA.str.len() > 1]
-train_features, test_features, train_labels, test_labels = sklearn.model_selection.train_test_split(data[['SOSPECHA_DIAGNOSTICA','edad']], data['GES'], test_size = 0.1, random_state = 42)
+# train_features, test_features, train_labels, test_labels = sklearn.model_selection.train_test_split(data[['SOSPECHA_DIAGNOSTICA','edad']], data['GES'], test_size = 0.1, random_state = 42)
 
-print(str(train_labels.count()) + ' train data points')
-print(str(test_labels.count()) + ' test data points')
+# print(str(train_labels.count()) + ' train data points')
+# print(str(test_labels.count()) + ' test data points')
 
-print(train_labels.value_counts())
+# print(train_labels.value_counts())
 
-print('balancing training subset')
+# print('balancing training subset')
 
-train = pd.concat([train_features,train_labels], axis=1)
-count_class_0, count_class_1 = train['GES'].value_counts()
-df_class_0 = train[train['GES'] == False]
-df_class_1 = train[train['GES'] == True]
-df_class_0_under = df_class_0.sample(count_class_1)
-train_under = pd.concat([df_class_0_under, df_class_1], axis=0)
-train_features = train_under[['SOSPECHA_DIAGNOSTICA','edad']]
-train_labels = train_under['GES']
+# train = pd.concat([train_features,train_labels], axis=1)
+# count_class_0, count_class_1 = train['GES'].value_counts()
+# df_class_0 = train[train['GES'] == False]
+# df_class_1 = train[train['GES'] == True]
+# df_class_0_under = df_class_0.sample(count_class_1)
+# train_under = pd.concat([df_class_0_under, df_class_1], axis=0)
+# train_features = train_under[['SOSPECHA_DIAGNOSTICA','edad']]
+# train_labels = train_under['GES']
 
-print(train_labels.value_counts())
+# print(train_labels.value_counts())
 
-print('saving subsets')
+# print('saving subsets')
 
-train_age = train_features['edad']
-train_text = train_features['SOSPECHA_DIAGNOSTICA'].tolist()
-train_text = [' '.join(sentence) for sentence in train_text]
+# train_age = train_features['edad']
+# train_text = train_features['SOSPECHA_DIAGNOSTICA'].tolist()
+# train_text = [' '.join(sentence) for sentence in train_text]
 
-test_age = test_features['edad']
-test_text = test_features['SOSPECHA_DIAGNOSTICA'].tolist()
-test_text = [' '.join(sentence) for sentence in test_text]
+# test_age = test_features['edad']
+# test_text = test_features['SOSPECHA_DIAGNOSTICA'].tolist()
+# test_text = [' '.join(sentence) for sentence in test_text]
 
-train_age.to_csv(r'../../data/processed/' + 'train_age.txt', index=False)
-train_labels.to_csv(r'../../data/processed/' + 'train_labels.txt', index=False)
-with open(r'../../data/interim/' + 'train_text.txt', 'w', encoding='utf-8') as file:
-    for sentence in train_text:
-        file.write(sentence)
-        file.write('\n')
+# train_age.to_csv(r'../../data/processed/' + 'train_age.txt', index=False)
+# train_labels.to_csv(r'../../data/processed/' + 'train_labels.txt', index=False)
+# with open(r'../../data/interim/' + 'train_text.txt', 'w', encoding='utf-8') as file:
+#     for sentence in train_text:
+#         file.write(sentence)
+#         file.write('\n')
 
-test_age.to_csv(r'../../data/processed/' + 'test_age.txt', index=False)
-test_labels.to_csv(r'../../data/processed/' + 'test_labels.txt', index=False)
-with open(r'../../data/interim/' + 'test_text.txt', 'w', encoding='utf-8') as file:
-    for sentence in test_text:
+# test_age.to_csv(r'../../data/processed/' + 'test_age.txt', index=False)
+# test_labels.to_csv(r'../../data/processed/' + 'test_labels.txt', index=False)
+# with open(r'../../data/interim/' + 'test_text.txt', 'w', encoding='utf-8') as file:
+#     for sentence in test_text:
+#         file.write(sentence)
+#         file.write('\n')
+
+ages = data['edad']
+texts = data['SOSPECHA_DIAGNOSTICA'].tolist()
+texts = [' '.join(sentence) for sentence in texts]
+labels = data['GES']
+
+ages.to_csv(r'../../data/processed/' + 'ages.txt', index=False)
+labels.to_csv(r'../../data/processed/' + 'labels.txt', index=False)
+with open(r'../../data/interim/' + 'texts.txt', 'w', encoding='utf-8') as file:
+    for sentence in texts:
         file.write(sentence)
         file.write('\n')
