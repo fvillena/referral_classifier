18ca91b254d3315ccd803e61b4bded36c1498a0a
Fabián Villena <villenafabian@gmail.com>
2019-10-04 13:22:55 +0200

    ges dataset

--- analyze.py
+++ analyze.py
@@ -1,7 +1,13 @@
 from src.data.text_data import CorpusGenerator
+from src.data.ges_data import GesDatasetGenerator
 
-generator = CorpusGenerator('data/raw/waiting_list_corpus_raw/','Rene Lagos - SELECT_ID_CORTA_FOLIO_INGRESO_GES_RUTPACIENTE_ESPECIALIDAD_FECHA_201810301333.csv')
-generator.load_files()
-generator.preprocess()
-generator.process()
-generator.write_corpus('data/interim/corpus.txt')
+corpus_generator = CorpusGenerator('data/raw/waiting_list_corpus_raw/','Rene Lagos - SELECT_ID_CORTA_FOLIO_INGRESO_GES_RUTPACIENTE_ESPECIALIDAD_FECHA_201810301333.csv')
+corpus_generator.load_files()
+corpus_generator.preprocess()
+corpus_generator.process()
+corpus_generator.write_corpus('data/interim/corpus.txt')
+
+ges_generator = GesDatasetGenerator('data/raw/waiting_list_corpus_raw/Rene Lagos - SELECT_ID_CORTA_FOLIO_INGRESO_GES_RUTPACIENTE_ESPECIALIDAD_FECHA_201810301333.csv')
+ges_generator.preprocess()
+ges_generator.split()
+ges_generator.write_dataset('data/processed/','data/interim/')
