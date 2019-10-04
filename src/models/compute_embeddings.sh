53ef79909ba66bf0dc96a96fc17caa2e48db0694
Fabián Villena <villenafabian@gmail.com>
2019-10-04 13:46:42 +0200

    changing file locations

--- src/models/compute_embeddings.sh
+++ src/models/compute_embeddings.sh
@@ -2,7 +2,7 @@
 
 word2vec_binary_location="/home/fvillena/word2vec/word2vec"
 corpus_location="/home/fvillena/code/referral_classifier/data/processed/corpus.txt"
-embedding_file="/home/fvillena/code/referral_classifier/models/embeddings/embeddings.vec"
+embedding_file="/home/fvillena/code/referral_classifier/models/embeddings.vec"
 threads=4
 
 $word2vec_binary_location -train $corpus_location -output $embedding_file -debug 2 -cbow 0 -size 300 -threads $threads -binary 0
