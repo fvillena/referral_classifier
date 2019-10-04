b22a3b47e3f6ed64093b173b100aba88359e0665
Fabián Villena <villenafabian@gmail.com>
2019-10-04 16:52:08 +0200

    n_jobs argument

--- src/models/ges_classifier.py
+++ src/models/ges_classifier.py
@@ -35,14 +35,14 @@ class GesModelTrainer:
                     self.train_labels.append(False)
         self.train_labels = np.asarray([self.train_labels]).T
         self.train = np.concatenate([self.train_texts, self.train_ages, self.train_labels], axis=1)
-    def train_models(self):
+    def train_models(self,n_jobs=4):
         self.scores = {}
         for model in self.models:
             clf = model
             clf_name = clf.__class__.__name__
             features = self.train[:,:-1]
             labels = self.train[:,-1]
-            self.scores[clf_name] = sklearn.model_selection.cross_validate(clf,features,labels,n_jobs=4,scoring=['f1_weighted','recall_weighted'],verbose=2,cv=10)
+            self.scores[clf_name] = sklearn.model_selection.cross_validate(clf,features,labels,n_jobs=n_jobs,scoring=['f1_weighted','recall_weighted'],verbose=2,cv=10)
     def generate_report(self,report_location):
         with open(report_location, 'w', encoding='utf-8') as json_file:
             json.dump(self.scores, json_file, indent=2, ensure_ascii=False)
