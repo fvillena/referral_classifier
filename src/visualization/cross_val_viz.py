b9ebbdf3b1710fc062d1325ca692b255699a7d2b
Fabián Villena <villenafabian@gmail.com>
2019-10-29 13:48:11 +0100

    sorted results and tilted xticks

--- src/visualization/cross_val_viz.py
+++ src/visualization/cross_val_viz.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 class CrossValVisualizer:
     def __init__(self,results_location):
@@ -13,8 +14,12 @@ class CrossValVisualizer:
                 self.scores_total[estimator_name] = data
             self.scores = [score['test_roc_auc'] for score in self.scores_total.values()]
             self.models = [model for model in self.scores_total.keys()]
+            self.sorted_data = [(model_name,scores,np.mean(scores)) for model_name,scores in zip (self.models, self.scores)]
+            self.sorted_data.sort(key=lambda tup: tup[2])
+            self.models, self.scores, _ = zip(*self.sorted_data)
     def plot(self,figure_location):
         plt.boxplot(self.scores, labels=self.models)
+        plt.xticks(rotation=45)
         plt.tight_layout()
         plt.savefig(figure_location)
 
